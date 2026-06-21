75bb74b3970ae5e3df80794c14946b54d31bc879
Zhang Huanjie <james@ustc.edu.cn>
2026-06-21 15:21:48 +0800

    fix: err_doit strcat 1-byte overflow

--- util.c
+++ util.c
@@ -59,9 +59,14 @@ void err_doit(int errnoflag, int level, const char *fmt, va_list ap)
 	errno_save = errno;	/* value caller might want printed */
 	vsnprintf(buf, sizeof(buf), fmt, ap);	/* this is safe */
 	n = strlen(buf);
-	if (errnoflag)
+	if (errnoflag) {
 		snprintf(buf + n, sizeof(buf) - n, ": %s", strerror(errno_save));
-	strcat(buf, "\n");
+		n = strlen(buf);
+	}
+	if (n < sizeof(buf) - 1) {
+		buf[n] = '\n';
+		buf[n + 1] = '\0';
+	}
 
 	if (daemon_proc)
 		syslog(level, "%s", buf);
